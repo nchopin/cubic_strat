Re-implementation of numdiff module (deriv of order 2 and 4 now work for d=2)   starts to get good result for d=2, but there may be a remaining bug (accuracy of hessian too low for orders=5, 6)

dick4D.py
@@ -9,7 +9,7 @@ orders = [1, 2, 3, 4, 5, 6]
 min_neval = 20
 max_neval = 10**7
 nks = 10
-nreps = 500
+nreps = 50
 
 def phi(u):
     return u[:, 1] * u[:, 2]**2 * u[:, 3]**3 * np.exp(np.prod(u, axis=-1))

numdiff.py
@@ -1,77 +1,76 @@
+"""
+Numerical derivatives with a twist:
 
-# Taken from Wikipedia: coefficients for O(h^8) numerical derivative of order 2
+* works for arrays;
+* compute all derivatives of order 2, 4 simultaneously;
+* accuracy is O(h^2).
 
-deriv_coeffs = {}
+"""
 
-deriv_coeffs[2] = {-4: -1/560, -3: 8/315, -2: -1/5, -1: 8/5, 
-                   0: -205/72, 1: 8/5, 2: -1/5, 3: 8/315, 4: -1/560}
+import numpy as np
 
-deriv_coeffs[4] = {
-    -5: -0.005423280423280352,
-    -4: 0.08339947089946993,
-    -3: -0.6440476190476121,
-    -2: 3.467460317460281,
-    -1: -9.272222222222187,
-    0: 12.741666666666717,
-    1: -9.272222222222323,
-    2: 3.4674603174603744,
-    3: -0.6440476190476302,
-    4: 0.08339947089947249,
-    5: -0.005423280423280538}
+def dx2(f, x, h):
+    der = (f(x + h) + f(x - h) - 2. * f(x)) / h**2
+    nevals = 3 * x.shape[0]
+    return der, nevals
 
-deriv_coeffs[6] = {
-    -6: -0.011491402116396317,
-    -5: 0.19206349206339682,
-    -4: -1.5500992063484338,
-    -3: 7.965608465604222,
-    -2: -25.18824404760001,
-    -1: 49.476190476111405,
-    0: -61.7680555554431,
-    1: 49.476190476135734,
-    2: -25.188244047614322,
-    3: 7.965608465606995,
-    4: -1.5500992063489303,
-    5: 0.1920634920634603,
-    6: -0.011491402116400401}
+def hessian(f, x, h):
+    def dv2(v):
+        return (f(x + v) + f(x - v) - tfx) / h**2
+    N, d = x.shape
+    hess = np.empty((N, d, d))
+    tfx = 2. * f(x)
+    iden = np.eye(d)
+    for i in range(d):
+        v = h * iden[i, :]
+        hess[:, i, i] = dv2(v)
+    for i in range(d):
+        for j in range(i):  # j<i
+            v = h * (iden[i, :] + iden[j, :])
+            hess[:, i, j] = 0.5 * (dv2(v) -hess[:, i, i] - hess[:, j, j])
+            hess[:, j, i] = hess[:, i, j]
+    nevals = N * (1 +  d * (d + 1))
+    return hess, nevals
 
-deriv_coeffs[8] = {
-    -7: -0.02056878306883161,
-    -6: 0.37407407407478294,
-    -5: -3.2384259259303967,
-    -4: 17.5037037037167,
-    -3: -62.5337962962762,
-    -2: 151.80370370324547,
-    -1: -255.9680555507601,
-    0: 304.1587301497311,
-    1: -255.9680555504124,
-    2: 151.80370370313918,
-    3: -62.533796296142555,
-    4: 17.50370370366318,
-    5: -3.2384259259177677,
-    6: 0.3740740740730166,
-    7: -0.020568783068717744}
+co = np.ones((3, 2))
+co[1, 0] = 2.
+co[2, 1] = 2. 
+Ainv = np.array([[10., -1., -1.], 
+                 [-6., 1., 0.5],
+                 [-6., 0.5, 1.]]) / 12.
 
-def dir_deriv(x, vec, k, h=0.01, phi=None):
-    """ Numerical snd derivative along a certain direction.
+def dx4(f, x, h):
+    deriv = (f(x + 2. * h) + f(x - 2. * h) - 4. * f(x + h) - 4. * f(x - h)
+             + 6. * f(x)) / h**4
+    nevals = 5 * x.shape[0]
+    return deriv, nevals
 
-    Approximates the k-th derivative of: 
-        t -> phi(x + t * vec)
-    where t is a scalar, and vec, x are in R^d, d>=2
-
-    """
-    der = sum(c * phi(x + o * h * vec) for o, c in deriv_coeffs[k].items())
-    return der / h**2
-
-def dx2dy2(x, i, j, d, h=0.01, phi=None):
-    """
-    Computes d^4 f / dx^2 dy^2 for function phi, where x/y=component i, j
-
-    """
-
-    hi = np.zeros(d); hi[i] = h  # directions
-    hj = np.zeros(d); hj[j] = h
-    s = (phi(x + hi + hj) + phi(x + hi - hj) 
-         + phi(x - hi + hj) + phi(x - hi - hj))
-    t = phi(x + hi) + phi(x - hi) + phi(x + hj) + phi(x - hj)
-    return (s - 2. * t + 4 * phi(x)) / h^4 
-
+def deriv4(f, x, h):
+    def dv4(v):
+        hv = h * v
+        return (1. / h**4) * (f(x + 2. * hv) + f(x - 2. * hv) 
+                              - 4. * f(x + hv) - 4. * f(x - hv) + sfx)
+    N, d = x.shape
+    if d != 2:
+        raise ValueError('deriv4: only dim=2 implemented')
+    D = np.empty((N, d, d, d, d))
+    sfx = 6. * f(x)
+    nevals = N
+    iden = np.eye(d)
+    for i in range(d):
+        D[:, i, i, i, i] = dv4(iden[i, :])
+        nevals += 4 * N
+    for i in range(d): 
+        for j in range(i):
+            dv = np.empty((N, 3))
+            for k in range(3):
+                v = co[k, 0] * iden[i, :] + co[k, 1] * iden[j, :]
+                dv[:, k] = (dv4(v) - co[k, 0]**4 * D[:, i, i, i, i] 
+                            - co[k, 1]**4 * D[:, j, j, j, j])
+                nevals += 4 * N
+                w = dv @ Ainv.T
+                D[:, i, i, j, j] = w[:, 0]
+                D[:, i, i, i, j] = w[:, 1]
+                D[:, i, j, j, j] = w[:, 2]
+    # TODO other derivatives 
+    return D, nevals

strat.py
@@ -13,6 +13,8 @@ from numpy import random
 from scipy import linalg
 from scipy.special import comb
 
+import numdiff
+
 MAX_SIZE = 10**5
 INT_TYPE = np.int32  # SIGNED INT, YOU DIM-WIT!!!
 # int type for arrays of indices; to save some space we do not use uint64
@@ -171,57 +173,13 @@ def vanish_estimates(k=10, d=2, order=1, phi=None):
 # General (non-vanishing) functions
 ###################################
 
-def numdx2(phi, x, i, h):
-    if x.ndim == 1:
-        N = x.shape[0]
-        hv = h
-    else:
-        N, d = x.shape
-        v = np.eye(d)[i]
-        hv = h * v
-    dx2 = (phi(x + hv) + phi(x - hv) - 2. * phi(x)) / h**2
-    nevals = 3 * N
-    return dx2, nevals
-
-def numdxdy(phi, x, i, j, h):
-    N, d = x.shape
-    ident = np.eye(d)
-    v = ident[i] + ident[j]
-    w = ident[i] - ident[j]
-    dxdy = (phi(x + h * v) + phi(x - h * v) - phi(x + h * w) - phi(x - h *w)
-           ) / (4 * h**2)
-    nevals = 4 * N
-    return dxdy, nevals
-
-def numdx4(phi, x, i, h):
-    if x.ndim == 1:
-        N = x.shape[0]
-        hv = h
-    else:
-        N, d = x.shape
-        v = np.eye(d)[i]
-        hv = h * v
-    thv = 2. * hv
-    dx4 = (phi(x - thv) - 4 * phi(x - hv) +  6. * phi(x) - 4. * phi(x + hv) +
-           phi(x + thv)) / h**4
-    nevals = 5 * N
-    return dx4, nevals
-
 def order2_correct(c, u, k, d, phi, deriv):
     if deriv is None:
-        h = 0.3 / k  # TODO
+        h = 0.4 / k
         if d == 1:
-            H, nevals = numdx2(phi, c, 0, h)
+            H, nevals = numdiff.dx2(phi, c, h)
         else:
-            nevals = 0
-            N, d = c.shape
-            H = np.zeros((N, d, d))
-            for i in range(d):
-                H[:, i, i], ne = numdx2(phi, c, i, h)
-                nevals += ne
-                for j in range(i):
-                    H[:, i, j], ne = numdxdy(phi, c, i, j, h)
-                    nevals += ne
+            H, nevals = numdiff.hessian(phi, c, h)
     else:
         H = deriv(2, c)
         nevals = c.shape[0] * ((d * (d + 1)) // 2)
@@ -244,11 +202,11 @@ def mult(ct, d):
 def order4_correct(c, u, k, d, phi, deriv):
     N = c.shape[0]
     if deriv is None:
-        h = 0.3 / k  # TODO
+        h = 0.2 / k  # TODO
         if d == 1:
-            D, nevals = numdx4(phi, c, 0, h)
+            D, nevals = numdiff.dx4(phi, c, h)
         else:
-            raise NotImplementedError
+            D, nevals = numdiff.deriv4(phi, c, h)
     else:
         D = deriv(4, c)
         nevals = N * comb(d + 3, 4, exact=True)
@@ -260,20 +218,20 @@ def order4_correct(c, u, k, d, phi, deriv):
         order4_term = 0.
         expect_order4 = 0.
         nevals = 0
-        for i in range(d):
-            for j in range(i+1):
-                for k in range(j+1):
-                    for l in range(k+1):
-                        ct = collections.Counter([i, j, k, l])
-                        uijkl = u[i, :] * u[j, :] * u[k, :] * u[l, :]
-                        mu = mult(ct, d)
-                        order4_term += mu * D[:, i, j, k, l] * uijkl
+        for g in range(d):
+            for h in range(g+1):
+                for i in range(h+1):
+                    for j in range(i+1):
+                        ct = Counter([g, h, i, j])
+                        uijkl = u[:, g] * u[:, h] * u[:, i] * u[:, j]
+                        mu = mult(ct, 4)
+                        order4_term += mu * D[:, g, h, i, j] * uijkl
                         # nevals += N
-                        if i==j==k==l:
+                        if g==h==i==j:
                             expect_order4 += (D[:, i, i, i, i] 
                                               * unif_mom[4] / k**4)
                         if all([v % 2 == 0 for v in ct.values()]):
-                            expect_order4 += (mu * D[:, i, j, k, l] *
+                            expect_order4 += (mu * D[:, g, h, i, j] *
                                                    unif_mom[2]**2 / k**4)
     correct = (expect_order4 - order4_term) / (2*3*4)
     return correct, nevals